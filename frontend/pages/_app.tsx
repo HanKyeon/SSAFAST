ceb2d1f1439a830a7c9a4628c34355c8fee0b806
HanKyeon <hgh21233@gmail.com>
2023-04-24 12:19:48 +0900

    [FE] Feat: RQ + Redux provider setting

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,42 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Toast from '@/components/common/Toast';
+import { wrapper } from '@/store';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useState } from 'react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const App = function ({ Component, ...rest }: AppProps) {
+  const { store, props } = wrapper.useWrappedStore(rest);
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: 1,
+            // useErrorBoundary: true,
+            // enabled: true,
+            // cacheTime: 10 * 60 * 1000,
+            // staleTime: 10 * 60 * 1000,
+            refetchOnWindowFocus: false,
+          },
+          mutations: {
+            // useErrorBoundary: true,
+          },
+        },
+      })
+  );
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Provider store={store}>
+        <Component {...props.pageProps}></Component>
+      </Provider>
+    </QueryClientProvider>
+  );
+};
+
+// wrapper의 withRedux로 해도 되고, Provider로 감싸줘도 된다. 나는 감싸주려한다.
+export default App;
--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,13 +1,29 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ko">
+      <Head>
+        <meta charSet="utf-8" />
+        {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
+        <meta name="theme-color" content="#1A1E1B" />
+        <meta property="og:locale" content="ko_KR" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="SSAFAST" />
+        <meta property="og:image" content="%PUBLIC_URL%/theFox.png" />
+        {/* <title>A502</title>
+        <meta name="description" content="SSAFAST, 빠른 프로젝트 개발 도우미" />
+        <meta property="og:url" content="https://j8a601.p.ssafy.io" />
+        <meta property="og:title" content="SSAFAST" />
+        <meta property="og:description" content="짱빠른 프로젝트 개발 도우미" />
+        <link rel="apple-touch-icon" href="%PUBLIC_URL%/theFox.png" /> */}
+      </Head>
       <body>
+        <div id="backdrop-root"></div>
+        <div id="overlay-root"></div>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
