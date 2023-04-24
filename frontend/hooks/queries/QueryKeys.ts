9c056ccb16a8e6ad7535e53b883ac40fda688529
HanKyeon <hgh21233@gmail.com>
2023-04-24 18:00:21 +0900

    [FE] Feat: Figma 관련 reactQuery 세팅

--- frontend/hooks/queries/QueryKeys.ts
+++ frontend/hooks/queries/QueryKeys.ts
@@ -1,1 +1,39 @@
-export const a = 1;
+let queryKeyMap = `
+Query Key               //                        // Mutation invalidate
+
+user                    // 유저 정보              // 
+  ㄴspace               // 유저 space 정보        // 
+    ㄴlist              // 유저가 가진 space list //
+    ㄴspaceId           // space 별 정보 디테일   //
+      ㄴpreview             //                        // preview에 쓰일 data들
+      ㄴapi               //                        // api 작성 관련 get
+      ㄴpages
+figma  피그마 파일 아이디
+  ㄴtokens      // 피그마 토큰들
+  ㄴfile     
+    ㄴall          // 피그마 파일의 depth 3까지 모든 데이터
+    ㄴsections     // 선택된 figma section들 데이터
+`;
+export const queryKeys = {
+  ////////////
+  /* 최상단 */
+  ////////////
+  user: () => [`user`] as const,
+  figma: (figmaId: string) => [`figma`, figmaId] as const,
+
+  ////////////
+  /* figma */
+  ////////////
+  figmaTokens: (figmaId: string) =>
+    [...queryKeys.figma(figmaId), `tokens`] as const,
+  figmaFiles: (figmaId: string) =>
+    [...queryKeys.figma(figmaId), `file`] as const,
+
+  /////////////////////
+  /* figmaFiles 하위 */
+  /////////////////////
+  figmaDatas: (figmaId: string) =>
+    [...queryKeys.figmaFiles(figmaId), `all`] as const,
+  figmaSections: (figmaId: string) =>
+    [...queryKeys.figmaFiles(figmaId), `sections`] as const,
+};
--- frontend/hooks/queries/queries.ts
+++ frontend/hooks/queries/queries.ts
@@ -1,1 +1,40 @@
+import { useQuery } from '@tanstack/react-query';
+import { queryKeys } from './QueryKeys';
+
 export const a = 1;
+
+export const useFigmaTokens = function (figmaId: string) {
+  return useQuery({
+    queryKey: queryKeys.figmaTokens(figmaId),
+    queryFn: async function () {
+      return;
+    },
+    onSuccess: function () {},
+    onError: function () {},
+    refetchOnMount: false,
+  });
+};
+
+export const useFigmaDatas = function (figmaId: string) {
+  return useQuery({
+    queryKey: queryKeys.figmaDatas(figmaId),
+    queryFn: async function () {
+      return;
+    },
+    onSuccess: function () {},
+    onError: function () {},
+    refetchOnMount: false,
+  });
+};
+
+export const useFigmaSections = function (figmaId: string) {
+  return useQuery({
+    queryKey: queryKeys.figmaSections(figmaId),
+    queryFn: async function () {
+      return;
+    },
+    onSuccess: function () {},
+    onError: function () {},
+    refetchOnMount: false,
+  });
+};
