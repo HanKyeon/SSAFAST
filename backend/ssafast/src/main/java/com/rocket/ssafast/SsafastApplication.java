4f67428e8d34d6b5795846667d8b7be92fc8a13b
dolpong_ <dolpong_@naver.com>
2023-04-27 17:04:51 +0900

    [BE] FEAT : 프로젝트 초기 설정 누락 파일 추가

---
+++ backend/ssafast/src/main/java/com/rocket/ssafast/SsafastApplication.java
@@ -0,0 +1,13 @@
+package com.rocket.ssafast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsafastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsafastApplication.class, args);
+	}
+
+}
---
+++ backend/ssafast/src/test/java/com/rocket/ssafast/SsafastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocket.ssafast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsafastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
