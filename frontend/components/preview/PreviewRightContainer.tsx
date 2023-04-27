23592adc02ac5c1e4e1687fa35cd3999af070c56
eomji <anipap2@naver.com>
2023-04-27 16:24:30 +0900

    [FE] Feat: previewContainer 구현중

---
+++ frontend/components/preview/PreviewRightContainer.tsx
@@ -0,0 +1,5 @@
+const PreviewRightContainer = function (): JSX.Element {
+  return <div className={'flex-[1]'}>previewrightcontainer</div>;
+};
+
+export default PreviewRightContainer;
--- frontend/pages/space/index.tsx
+++ frontend/pages/space/index.tsx
@@ -1,3 +1,6 @@
+import PreviewLeftContainer from '@/components/preview/PreviewLeftContainer';
+import PreviewRightContainer from '@/components/preview/PreviewRightContainer';
+import SpaceNavContainer from '@/components/preview/SpaceNavContainer';
 import { queryKeys } from '@/hooks/queries/QueryKeys';
 import { wrapper } from '@/store';
 import apiRequest from '@/utils/axios';
@@ -5,11 +8,13 @@ import { QueryClient, dehydrate, useQuery } from '@tanstack/react-query';
 import { GetServerSideProps } from 'next';
 import { ParsedUrlQuery } from 'querystring';
 
-const SpacePage = function () {
+const SpacePage = function (): JSX.Element {
   return (
-    <>
-      <div>스페이슨데요???</div>
-    </>
+    <div className={'flex'}>
+      <SpaceNavContainer />
+      <PreviewLeftContainer />
+      <PreviewRightContainer />
+    </div>
   );
 };
 
