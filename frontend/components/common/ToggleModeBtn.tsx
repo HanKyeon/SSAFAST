4f6fb06616f67fdcbdf8295da0ab89b8fcdd779a
bisous1519 <anipap2@naver.com>
2023-04-28 11:11:39 +0900

    [FE] Feat: darkmode toggle 버튼 구현

--- frontend/components/common/ToggleModeBtn.tsx
+++ frontend/components/common/ToggleModeBtn.tsx
@@ -12,12 +12,16 @@ const ToggleModeBtn = function (): JSX.Element {
   const styles = {
     toggleBg: 'w-[45px] h-[20px] bg-grayscale-deepdark rounded-[20px] relative',
     toggleCircle:
-      'w-[25px] h-[25px] bg-white rounded-full absolute right-0 top-[-2.5px] flex items-center justify-center',
+      'w-[25px] h-[25px] bg-white rounded-full absolute top-[-2.5px] flex items-center justify-center transition-all duration-500',
   };
 
   return (
     <div className={`${styles['toggleBg']}`} onClick={onClickToggle}>
-      <div className={`${styles['toggleCircle']}`}>
+      <div
+        className={`${styles['toggleCircle']} ${
+          isDark ? 'right-[0%]' : 'right-[100%] translate-x-[100%]'
+        }`}
+      >
         {isDark ? (
           <HiMoon className="text-grayscale-dark" />
         ) : (
