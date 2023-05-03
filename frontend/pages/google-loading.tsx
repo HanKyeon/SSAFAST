0c39dd1ccc92408e60a4e00e0e9c70bb5db90bdb
HanKyeon <hgh21233@gmail.com>
2023-05-03 10:22:15 +0900

    [FE] Feat: google oauth testing

--- frontend/pages/google-loading.tsx
+++ frontend/pages/google-loading.tsx
@@ -1,7 +1,14 @@
 import MetaHead from '@/components/common/MetaHead';
 import { SpinnerDots } from '@/components/common/Spinner';
+import { wrapper } from '@/store';
+import { tokenActions } from '@/store/token-slice';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import { useRouter } from 'next/router';
 
-const GoogleLoadingPage = function () {
+const GoogleLoadingPage = function (
+  props: InferGetServerSidePropsType<typeof getServerSideProps>
+) {
+  console.log(props);
   return (
     <>
       <MetaHead />
@@ -13,3 +20,20 @@ const GoogleLoadingPage = function () {
 };
 
 export default GoogleLoadingPage;
+
+export const getServerSideProps: GetServerSideProps =
+  wrapper.getServerSideProps(function (store) {
+    return async function (context) {
+      const access = context.req.headers.authorization;
+      if (access) {
+        store.dispatch(tokenActions.setAccessToken({ accessToken: access }));
+      }
+
+      return {
+        props: {
+          asdf: `asdf`,
+          access: access,
+        },
+      };
+    };
+  });
