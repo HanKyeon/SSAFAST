8f051ba6a404d14c4b6f173264396df18905bd31
angly97 <angly97@naver.com>
2023-05-04 15:20:31 +0900

    [BE] Fix : API 개별 요청 테스트 API 구현

--- backend/src/main/java/com/rocket/ssafast/apispec/controller/DtoController.java
+++ backend/src/main/java/com/rocket/ssafast/apispec/controller/DtoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.rocket.ssafast.apispec.dto.request.BodyDto;
+import com.rocket.ssafast.apispec.dto.request.JavaPoetBodyDto;
 import com.rocket.ssafast.apispec.service.JavaPoetService;
 import com.rocket.ssafast.exception.CustomException;
 import com.rocket.ssafast.exception.ErrorCode;
@@ -23,16 +23,17 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/api/dto")
 @RequiredArgsConstructor
 public class DtoController {
-
 	private final JavaPoetService javaPoetService;
 
 	@GetMapping("/class")
-	public ResponseEntity<?> getDtoClassCode(@RequestBody BodyDto bodyDto) {
+	public ResponseEntity<?> getDtoClassCode(@RequestBody JavaPoetBodyDto javaPoetBodyDto) {
 		try {
-			String dtoClass = javaPoetService.generateDtoClassCode(bodyDto);
+			String dtoClass = javaPoetService.generateDtoClassCode(javaPoetBodyDto);
 			Map<String, String> result = new HashMap<>();
 			result.put("dtoClass", dtoClass);
 			return new ResponseEntity<>(result, HttpStatus.OK);
+		} catch (CustomException e) {
+			return new ResponseEntity<>(e.getMessage(), e.getHttpStatus());
 		} catch (Exception e) {
 			log.error("error: ", e);
 			ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
--- backend/src/main/java/com/rocket/ssafast/apispec/domain/Enum/HTTPMethod.java
+++ backend/src/main/java/com/rocket/ssafast/apispec/domain/Enum/HTTPMethod.java
@@ -8,17 +8,20 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.springframework.http.HttpMethod;
+
 @AllArgsConstructor
 public enum HTTPMethod {
 
-    GET(1, "GET"),
-    POST(2, "POST"),
-    PUT(3, "PUT"),
-    DELETE(4, "DELETE"),
-    PATCH(5, "PATCH");
+    GET(1, "GET", HttpMethod.GET),
+    POST(2, "POST", HttpMethod.POST),
+    PUT(3, "PUT", HttpMethod.PUT),
+    DELETE(4, "DELETE", HttpMethod.DELETE),
+    PATCH(5, "PATCH", HttpMethod.PATCH);
 
     private int number;
     private String status;
+    private HttpMethod method;
 
     private static final Map<String, Integer> BY_STATUS =
             Stream.of(values()).collect(
@@ -26,14 +29,22 @@ public enum HTTPMethod {
                             Collectors.toMap(HTTPMethod::status, HTTPMethod::number), Collections::unmodifiableMap));
 
     private static final Map<Integer, String> BY_NUMBER =
-            Stream.of(values()).collect(
-                    Collectors.collectingAndThen(
-                            Collectors.toMap(HTTPMethod::number, HTTPMethod::status), Collections::unmodifiableMap));
+        Stream.of(values()).collect(
+            Collectors.collectingAndThen(
+                Collectors.toMap(HTTPMethod::number, HTTPMethod::status), Collections::unmodifiableMap));
+
+    private static final Map<String, HttpMethod> METHOD_BY_STATUS =
+        Stream.of(values()).collect(
+            Collectors.collectingAndThen(
+                Collectors.toMap(HTTPMethod::status, HTTPMethod::method), Collections::unmodifiableMap));
 
 
     public int number(){ return number; }
     public String status() { return status; }
+    public HttpMethod method() { return method; }
 
     public static String getStatusByNumber(int number){ return BY_NUMBER.get(number).toString(); }
     public static int getNumberByStatus(String status) { return BY_STATUS.get(status); }
+    public static HttpMethod getMethodByStatus(String status) { return METHOD_BY_STATUS.get(status); }
 }
+
