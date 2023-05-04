dded4cd9a8471c96a6baf2030aec5f3c8ca4a57d
dolpong_ <dolpong_@naver.com>
2023-05-04 13:02:04 +0900

    [BE] Feat : ApiFigmaSection 컨트롤러 생성

--- backend/src/main/java/com/rocket/ssafast/apispec/controller/ApiFigmaSectionController.java
+++ backend/src/main/java/com/rocket/ssafast/apispec/controller/ApiFigmaSectionController.java
@@ -1,5 +1,36 @@
 package com.rocket.ssafast.apispec.controller;
 
+import com.rocket.ssafast.apispec.service.ApiFigmaSectionService;
+import com.rocket.ssafast.exception.CustomException;
+import com.rocket.ssafast.exception.ErrorCode;
+import com.rocket.ssafast.workspace.dto.request.CreateFigmaSectionDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+
+@Slf4j//log 객체 자동으로 생성
+@RequiredArgsConstructor//final로 선언된 필드들에 대한 생성자를 자동으로 생성
+@RestController//@Controller 어노테이션과 달리, @ResponseBody 어노테이션이 자동으로 적용
+@RequestMapping("/api/api-pre/figma-section")
 public class ApiFigmaSectionController {
 
+    private ApiFigmaSectionService apiFigmaSectionService;
+    private String SUCCESS = "SUCCESS";
+    @PostMapping("")
+    public ResponseEntity<?> createApiFigmaSection(@RequestParam Long figmaSectionId, @RequestBody HashMap<String, List<Long>> apiIds){
+        try {
+
+            return new ResponseEntity<>(apiFigmaSectionService.createApiFigmaSection(figmaSectionId, apiIds), HttpStatus.OK);
+        } catch (CustomException e){
+            return new ResponseEntity<>(e.getMessage(), e.getHttpStatus());
+        } catch (Exception e) {
+            log.error("error: ", e);
+            return new ResponseEntity<>(ErrorCode.INTERNAL_SERVER_ERROR.getMessage(), ErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus());
+        }
+    }
 }
