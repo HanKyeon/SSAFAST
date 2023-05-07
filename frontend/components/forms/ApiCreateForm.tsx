0bd7443c8cdbf79ca6a6d6dbf8fb5da645e4cdc5
YongjaeKwon <109266749+YongjaeKwon@users.noreply.github.com>
2023-05-07 20:55:35 +0900

    [FE] Form 형태 수정중

--- frontend/components/forms/ApiCreateForm.tsx
+++ frontend/components/forms/ApiCreateForm.tsx
@@ -1,23 +1,15 @@
-import {
-  FormProvider,
-  useForm,
-  Controller,
-  useFieldArray,
-} from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { Box, Button, CircleBtn } from '../common';
-import { useEffect, useRef, useState } from 'react';
-import useInput from '@/hooks/useInput';
-import useInputNumber from '@/hooks/useInputNumber';
-import ResponseForm from './ResponseForm';
-import RequestForm from './RequestForm';
+import { useEffect, useState } from 'react';
+import { BaseForm, RequestForm, ResponseForm } from './index';
 import { useStoreSelector } from '@/hooks/useStore';
 import AnimationBox from '../common/AnimationBox';
 import { IoMdArrowRoundBack } from 'react-icons/io';
-interface ApiCreateForm {
-  request: any;
-  response: any;
-}
+import { ResponseFormData } from './ResponseForm';
+import { RequestFormData } from './RequestForm';
+import { BaseFormData } from './BaseForm';
 
+// Form Tab 스타일
 const selectedStyle = (dark: boolean) =>
   `${
     dark
@@ -25,48 +17,46 @@ const selectedStyle = (dark: boolean) =>
       : 'border-b-[3px] border-taro-strong active:border-opacity-100 border-opacity-80 text-taro-strong active:bg-grayscale-light active:bg-opacity-70 active:border-taro-normal active:border-b-[3px] active:text-taro-normal'
   }` as const;
 
+// Form을 모을 최상위 함수
 const ApiCreateForm = function () {
-  const methods = useForm<ApiCreateForm>({
-    defaultValues: {
-      response: [],
-    },
-  });
-
+  const baseMethods = useForm<BaseFormData>();
+  const responseMethods = useForm<ResponseFormData>();
+  const requestMethods = useForm<RequestFormData>();
   const { dark } = useStoreSelector((state) => state.dark);
   const [step, setStep] = useState<number>(1);
 
+  // Request Tab 이동
   const requestTabHandler = function () {
     setStep(() => 1);
   };
+  // Response Tab 이동
   const responseTabHandler = function () {
     setStep(() => 2);
   };
 
-  const [requestData, setRequestData] = useState<any>();
-  const [responseData, setResponseData] = useState<any>();
-  const requestGetter = function (data: any) {
-    setRequestData(() => data);
+  // base form data 가져오는 함수
+  const baseGetter = function () {
+    baseMethods.getValues();
   };
-  const responseGetter = function (data: any) {
-    setResponseData(() => data);
+  // request form data 가져오는 함수
+  const requestGetter = function () {
+    requestMethods.getValues();
   };
-  const onSubmit = function (data: any) {
-    requestGetter(data);
-    responseGetter(data);
+  // response form data 가져오는 함수
+  const responseGetter = function () {
+    responseMethods.getValues();
   };
-  useEffect(
-    function () {
-      console.log(requestData);
-    },
-    [requestData]
-  );
-  useEffect(
-    function () {
-      console.log('이건 API 에서 찍힌 콘솔');
-      console.log(responseData);
-    },
-    [responseData]
-  );
+
+  // 폼 저장시 최종 호출을 할 버튼 함수.
+  const onSubmit = function () {
+    baseGetter();
+    requestGetter();
+    responseGetter();
+    console.log('베이스', baseMethods.getValues());
+    console.log('리퀘스트', requestMethods.getValues());
+    console.log('리스폰스', responseMethods.getValues());
+  };
+
   return (
     <>
       <div>
@@ -74,45 +64,44 @@ const ApiCreateForm = function () {
           <IoMdArrowRoundBack /> Back
         </Button>
       </div>
-      <FormProvider {...methods}>
-        <Box
-          fontType="normal"
-          className="flex justify-around flex-row items-center box-border pt-5"
-        >
-          <div
-            className={`${
-              step === 1 ? selectedStyle(dark) : ''
-            } cursor-pointer `}
-            onClick={requestTabHandler}
-          >
-            Request
-          </div>
-          <div
-            className={`${
-              step === 2 ? selectedStyle(dark) : ''
-            } cursor-pointer `}
-            onClick={responseTabHandler}
-          >
-            Response
-          </div>
-        </Box>
 
-        <AnimationBox
-          className={`${
-            step === 1 ? '' : 'hidden'
-          } flex w-full h-full items-center justify-center`}
+      <Box
+        fontType="normal"
+        className="flex justify-around flex-row items-center box-border pt-5"
+      >
+        <div
+          className={`${step === 1 ? selectedStyle(dark) : ''} cursor-pointer `}
+          onClick={requestTabHandler}
         >
-          <RequestForm requestGetter={requestGetter} />
-        </AnimationBox>
+          Request
+        </div>
+        <div
+          className={`${step === 2 ? selectedStyle(dark) : ''} cursor-pointer `}
+          onClick={responseTabHandler}
+        >
+          Response
+        </div>
+      </Box>
+      <button onClick={onSubmit}>최종 제출</button>
+      <AnimationBox
+        className={`${
+          step === 1 ? '' : 'hidden'
+        } flex flex-col w-full h-full justify-center`}
+      >
+        <BaseForm methods={baseMethods} baseGetter={baseGetter} />
+        <RequestForm methods={requestMethods} requestGetter={requestGetter} />
+      </AnimationBox>
 
-        <AnimationBox
-          className={`${
-            step === 2 ? '' : 'hidden'
-          } flex w-full h-full justify-center`}
-        >
-          <ResponseForm responseGetter={responseGetter} />
-        </AnimationBox>
-      </FormProvider>
+      <AnimationBox
+        className={`${
+          step === 2 ? '' : 'hidden'
+        } flex w-full h-full justify-center`}
+      >
+        <ResponseForm
+          methods={responseMethods}
+          responseGetter={responseGetter}
+        />
+      </AnimationBox>
     </>
   );
 };
--- frontend/components/forms/index.ts
+++ frontend/components/forms/index.ts
@@ -1,3 +1,4 @@
+export { default as BaseForm } from './BaseForm';
 export { default as RequestForm } from './RequestForm';
 export { default as ResponseForm } from './ResponseForm';
 export { default as DtoForm } from './DtoForm';
