15e9169b948c9c910edb4687458f548efb3569e2
YongjaeKwon <109266749+YongjaeKwon@users.noreply.github.com>
2023-05-09 07:44:53 +0900

    [FE] Form Style 작업중

--- frontend/components/forms/RequestForm.tsx
+++ frontend/components/forms/RequestForm.tsx
@@ -10,12 +10,12 @@ import { Box, Button, CircleBtn, Input, Select } from '../common';
 import { FormEvent, useRef, useState } from 'react';
 import useInput from '@/hooks/useInput';
 import useInputNumber from '@/hooks/useInputNumber';
-import { useStoreDispatch } from '@/hooks/useStore';
+import { useStoreDispatch, useStoreSelector } from '@/hooks/useStore';
 import { DispatchToast } from '@/store';
 import { inputTheme } from '@/utils/styleClasses';
 import { ApiCreateForm } from './ApiCreateForm';
 import ToggleableHeader from '../work/APIDocsContainer/ToggleableHeader';
-import ReqItemInner from '../work/APIDocsContainer/ReqItemInner';
+
 export interface RequestFormData {
   additional_url: string;
   headers: Headers[];
@@ -103,6 +103,9 @@ function PathVariableFields({ control }: { control: any }) {
       constraints: [],
       value: null,
     });
+    if (isOpen === false) {
+      setisOpen((prev) => !prev);
+    }
   };
 
   return (
@@ -204,6 +207,7 @@ function PathVariableFields({ control }: { control: any }) {
 }
 
 function ParamsFields({ control }: { control: any }) {
+  const [isOpen, setisOpen] = useState<boolean>(true);
   const {
     fields: paramsFields,
     append,
@@ -222,84 +226,102 @@ function ParamsFields({ control }: { control: any }) {
       constraints: [],
       value: null,
     });
+    if (isOpen === false) {
+      setisOpen((prev) => !prev);
+    }
   };
 
   return (
     <>
-      <div className="flex justify-between w-40">
-        <div>Params</div>
+      <div className="flex items-center w-96">
+        <ToggleableHeader
+          title="Params"
+          isOpen={isOpen}
+          setIsOpen={setisOpen}
+        />
         <CircleBtn
           btnType="plus"
           type="button"
           onClick={appendParamsInput}
         ></CircleBtn>
       </div>
-      {paramsFields.map((item, index) => (
-        <div key={item.id}>
-          <CircleBtn btnType="delete" onClick={() => remove(index)}></CircleBtn>
-          <Controller
-            name={`document.request.params.[${index}].key`}
-            control={control}
-            rules={{ required: true }}
-            render={({ field, fieldState }) => (
-              <div className="flex">
-                <label htmlFor={`params.[${index}].key`}>Key:</label>
-                <input type="text" id={`params.[${index}].key`} {...field} />
-                {fieldState?.invalid && <span>This field is required</span>}
-              </div>
-            )}
-          />
-          <Controller
-            name={`document.request.params.[${index}].type`}
-            control={control}
-            rules={{ required: true }}
-            render={({ field, fieldState }) => (
-              <div className="flex">
-                <label htmlFor={`params.[${index}].type`}>Type:</label>
-                <input type="text" id={`params.[${index}].type`} {...field} />
-                {fieldState?.invalid && <span>This field is required</span>}
-              </div>
-            )}
-          />
-          <Controller
-            name={`document.request.params.[${index}].desc`}
-            control={control}
-            rules={{ required: true }}
-            render={({ field, fieldState }) => (
-              <>
+      {isOpen &&
+        paramsFields.map((item, index) => (
+          <div key={item.id}>
+            <CircleBtn
+              btnType="delete"
+              onClick={() => remove(index)}
+            ></CircleBtn>
+            <Controller
+              name={`document.request.params.[${index}].key`}
+              control={control}
+              rules={{ required: true }}
+              render={({ field, fieldState }) => (
                 <div className="flex">
-                  <label htmlFor={`params.[${index}].desc`}>Description:</label>
-                  <input type="text" id={`params.[${index}].desc`} {...field} />
+                  <label htmlFor={`params.[${index}].key`}>Key:</label>
+                  <input type="text" id={`params.[${index}].key`} {...field} />
                   {fieldState?.invalid && <span>This field is required</span>}
                 </div>
-              </>
-            )}
-          />
-          <Controller
-            name={`document.request.params.[${index}].constraints`}
-            control={control}
-            render={({ field, fieldState }) => (
-              <>
+              )}
+            />
+            <Controller
+              name={`document.request.params.[${index}].type`}
+              control={control}
+              rules={{ required: true }}
+              render={({ field, fieldState }) => (
                 <div className="flex">
-                  <label htmlFor={`params.[${index}].constraints`}>
-                    Contstraints:
-                  </label>
-                  <input
-                    type="text"
-                    id={`params.[${index}].constraints`}
-                    {...field}
-                  />
+                  <label htmlFor={`params.[${index}].type`}>Type:</label>
+                  <input type="text" id={`params.[${index}].type`} {...field} />
                   {fieldState?.invalid && <span>This field is required</span>}
                 </div>
-              </>
-            )}
-          />
-        </div>
-      ))}
+              )}
+            />
+            <Controller
+              name={`document.request.params.[${index}].desc`}
+              control={control}
+              rules={{ required: true }}
+              render={({ field, fieldState }) => (
+                <>
+                  <div className="flex">
+                    <label htmlFor={`params.[${index}].desc`}>
+                      Description:
+                    </label>
+                    <input
+                      type="text"
+                      id={`params.[${index}].desc`}
+                      {...field}
+                    />
+                    {fieldState?.invalid && <span>This field is required</span>}
+                  </div>
+                </>
+              )}
+            />
+            <Controller
+              name={`document.request.params.[${index}].constraints`}
+              control={control}
+              render={({ field, fieldState }) => (
+                <>
+                  <div className="flex">
+                    <label htmlFor={`params.[${index}].constraints`}>
+                      Contstraints:
+                    </label>
+                    <input
+                      type="text"
+                      id={`params.[${index}].constraints`}
+                      {...field}
+                    />
+                    {fieldState?.invalid && <span>This field is required</span>}
+                  </div>
+                </>
+              )}
+            />
+          </div>
+        ))}
     </>
   );
 }
 function HeaderFields({ control }: { control: any }) {
+  const [isOpen, setisOpen] = useState<boolean>(true);
   const {
     fields: headerFields,
     append,
@@ -315,63 +337,72 @@ function HeaderFields({ control }: { control: any }) {
       type: '',
       desc: '',
     });
+    if (isOpen === false) {
+      setisOpen((prev) => !prev);
+    }
   };
   return (
     <>
-      <div className="flex justify-between w-40">
-        <div>Header</div>
+      <div className="flex items-center w-96">
+        <ToggleableHeader
+          title="Headers"
+          isOpen={isOpen}
+          setIsOpen={setisOpen}
+        />
         <CircleBtn btnType="plus" onClick={appendHeaderInput}></CircleBtn>
       </div>
-      {headerFields.map((item, index) => (
-        <div key={item.id} className="">
-          <CircleBtn
-            btnType="delete"
-            type="button"
-            onClick={() => remove(index)}
-          ></CircleBtn>
-          <Controller
-            name={`document.request.headers[${index}].key`}
-            control={control}
-            rules={{ required: true }}
-            render={({ field, fieldState }) => (
-              <div className="flex">
-                <label htmlFor={`headers[${index}].key`}>Key:</label>
-                <input type="text" id={`headers[${index}].key`} {...field} />
-                {fieldState?.invalid && <span>This field is required</span>}
-              </div>
-            )}
-          />
-          <Controller
-            name={`document.request.headers[${index}].type`}
-            control={control}
-            rules={{ required: true }}
-            render={({ field, fieldState }) => (
-              <div className="flex">
-                <label htmlFor={`headers[${index}].type`}>Type:</label>
-                <input type="text" id={`headers[${index}].type`} {...field} />
-                {fieldState?.invalid && <span>This field is required</span>}
-              </div>
-            )}
-          />
-          <Controller
-            name={`document.request.headers[${index}].desc`}
-            control={control}
-            rules={{ required: true }}
-            render={({ field, fieldState }) => (
-              <>
-                <label htmlFor={`headers[${index}].desc`}>Description:</label>
-                <input type="text" id={`headers[${index}].desc`} {...field} />
-                {fieldState?.invalid && <span>This field is required</span>}
-              </>
-            )}
-          />
-        </div>
-      ))}
+      {isOpen &&
+        headerFields.map((item, index) => (
+          <div key={item.id} className="">
+            <CircleBtn
+              btnType="delete"
+              type="button"
+              onClick={() => remove(index)}
+            ></CircleBtn>
+            <Controller
+              name={`document.request.headers[${index}].key`}
+              control={control}
+              rules={{ required: true }}
+              render={({ field, fieldState }) => (
+                <div className="flex">
+                  <label htmlFor={`headers[${index}].key`}>Key:</label>
+                  <input type="text" id={`headers[${index}].key`} {...field} />
+                  {fieldState?.invalid && <span>This field is required</span>}
+                </div>
+              )}
+            />
+            <Controller
+              name={`document.request.headers[${index}].type`}
+              control={control}
+              rules={{ required: true }}
+              render={({ field, fieldState }) => (
+                <div className="flex">
+                  <label htmlFor={`headers[${index}].type`}>Type:</label>
+                  <input type="text" id={`headers[${index}].type`} {...field} />
+                  {fieldState?.invalid && <span>This field is required</span>}
+                </div>
+              )}
+            />
+            <Controller
+              name={`document.request.headers[${index}].desc`}
+              control={control}
+              rules={{ required: true }}
+              render={({ field, fieldState }) => (
+                <>
+                  <label htmlFor={`headers[${index}].desc`}>Description:</label>
+                  <input type="text" id={`headers[${index}].desc`} {...field} />
+                  {fieldState?.invalid && <span>This field is required</span>}
+                </>
+              )}
+            />
+          </div>
+        ))}
     </>
   );
 }
 
 function BodyFields({ control }: { control: any }) {
+  const [isOpen, setisOpen] = useState<boolean>(true);
   const {
     fields: bodyFields,
     append,
@@ -391,108 +422,117 @@ function BodyFields({ control }: { control: any }) {
       constraints: [],
       value: null,
     });
+    if (isOpen === false) {
+      setisOpen((prev) => !prev);
+    }
   };
 
   return (
     <>
-      <div className="flex justify-between w-40">
-        <div>body</div>
+      <div className="flex items-center w-96">
+        <ToggleableHeader title="Body" isOpen={isOpen} setIsOpen={setisOpen} />
         <CircleBtn
           btnType="plus"
           type="button"
           onClick={appendBodyInput}
         ></CircleBtn>
       </div>
-      {bodyFields.map((item, index) => (
-        <div key={item.id}>
-          <CircleBtn btnType="delete" onClick={() => remove(index)}></CircleBtn>
-          <Controller
-            name={`document.request.body.fields[${index}].key`}
-            control={control}
-            rules={{ required: true }}
-            render={({ field, fieldState }) => (
-              <div className="flex">
-                <label htmlFor={`bbody.fields[${index}].key`}>Key:</label>
-                <input
-                  type="text"
-                  id={`body.fields[${index}].key`}
-                  {...field}
-                />
-                {fieldState?.invalid && <span>This field is required</span>}
-              </div>
-            )}
-          />
-          <Controller
-            name={`document.request.body.fields[${index}].type`}
-            control={control}
-            rules={{ required: true }}
-            render={({ field, fieldState }) => (
-              <div className="flex">
-                <label htmlFor={`body.fields[${index}].type`}>Type:</label>
-                <input
-                  type="text"
-                  id={`body.fields[${index}].type`}
-                  {...field}
-                />
-                {fieldState?.invalid && <span>This field is required</span>}
-              </div>
-            )}
-          />
-          <Controller
-            name={`document.request.body.fields[${index}].desc`}
-            control={control}
-            rules={{ required: true }}
-            render={({ field, fieldState }) => (
-              <>
+      {isOpen &&
+        bodyFields.map((item, index) => (
+          <div key={item.id}>
+            <CircleBtn
+              btnType="delete"
+              onClick={() => remove(index)}
+            ></CircleBtn>
+            <Controller
+              name={`document.request.body.fields[${index}].key`}
+              control={control}
+              rules={{ required: true }}
+              render={({ field, fieldState }) => (
                 <div className="flex">
-                  <label htmlFor={`body.fields[${index}].desc`}>
-                    Description:
-                  </label>
+                  <label htmlFor={`bbody.fields[${index}].key`}>Key:</label>
                   <input
                     type="text"
-                    id={`body.fields[${index}].desc`}
+                    id={`body.fields[${index}].key`}
                     {...field}
                   />
                   {fieldState?.invalid && <span>This field is required</span>}
                 </div>
-              </>
-            )}
-          />
-          <Controller
-            name={`document.request.body.fields[${index}].constraints`}
-            control={control}
-            render={({ field }) => (
-              <>
+              )}
+            />
+            <Controller
+              name={`document.request.body.fields[${index}].type`}
+              control={control}
+              rules={{ required: true }}
+              render={({ field, fieldState }) => (
                 <div className="flex">
-                  <label htmlFor={`body.fields[${index}].constraints`}>
-                    Constraints:
-                  </label>
+                  <label htmlFor={`body.fields[${index}].type`}>Type:</label>
                   <input
                     type="text"
-                    id={`body.fields[${index}].constraints`}
+                    id={`body.fields[${index}].type`}
                     {...field}
                   />
+                  {fieldState?.invalid && <span>This field is required</span>}
                 </div>
-              </>
-            )}
-          />
-          <Controller
-            name={`document.request.body.fields[${index}].itera`}
-            control={control}
-            // rules={{ required: true }}
-            render={({ field }) => (
-              <>
-                <label htmlFor={`body.fields[${index}].itera`}>Is List?:</label>
-                <input
-                  type="checkbox"
-                  id={`body.fields[${index}].itera`}
-                  {...field}
-                />
-              </>
-            )}
-          />
-        </div>
-      ))}
+              )}
+            />
+            <Controller
+              name={`document.request.body.fields[${index}].desc`}
+              control={control}
+              rules={{ required: true }}
+              render={({ field, fieldState }) => (
+                <>
+                  <div className="flex">
+                    <label htmlFor={`body.fields[${index}].desc`}>
+                      Description:
+                    </label>
+                    <input
+                      type="text"
+                      id={`body.fields[${index}].desc`}
+                      {...field}
+                    />
+                    {fieldState?.invalid && <span>This field is required</span>}
+                  </div>
+                </>
+              )}
+            />
+            <Controller
+              name={`document.request.body.fields[${index}].constraints`}
+              control={control}
+              render={({ field }) => (
+                <>
+                  <div className="flex">
+                    <label htmlFor={`body.fields[${index}].constraints`}>
+                      Constraints:
+                    </label>
+                    <input
+                      type="text"
+                      id={`body.fields[${index}].constraints`}
+                      {...field}
+                    />
+                  </div>
+                </>
+              )}
+            />
+            <Controller
+              name={`document.request.body.fields[${index}].itera`}
+              control={control}
+              // rules={{ required: true }}
+              render={({ field }) => (
+                <>
+                  <label htmlFor={`body.fields[${index}].itera`}>
+                    Is List?:
+                  </label>
+                  <input
+                    type="checkbox"
+                    id={`body.fields[${index}].itera`}
+                    {...field}
+                  />
+                </>
+              )}
+            />
+          </div>
+        ))}
     </>
   );
 }
