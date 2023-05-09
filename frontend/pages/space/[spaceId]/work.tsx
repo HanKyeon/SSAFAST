225d1d63ba243fe674079282bf96f4eaf08e899b
HanKyeon <hgh21233@gmail.com>
2023-05-09 13:29:28 +0900

    [FE] Feat: change ws link

--- frontend/pages/space/[spaceId]/work.tsx
+++ frontend/pages/space/[spaceId]/work.tsx
@@ -59,7 +59,7 @@ const SpaceWorkPage = function (
         `ssafast${spaceId}`,
         getYjsDoc(state) as any,
         {
-          signaling: [`ws://localhost:4444`],
+          signaling: [`ws://ssafast.com:4444`],
         }
       );
       console.log('커넥트');
