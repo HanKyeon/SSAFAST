10a427fadec5caf4ff4b4bfde96edb04fa620229
mickeyshoes <parody9c@gmail.com>
2023-05-18 22:48:50 +0900

    [BE] Fix : dto 수정 시 keyname, desc, constraints update 안되는거 추가

--- backend/src/main/java/com/rocket/ssafast/dtospec/dto/request/AddDtoSpecDto.java
+++ backend/src/main/java/com/rocket/ssafast/dtospec/dto/request/AddDtoSpecDto.java
@@ -1,15 +1,14 @@
 package com.rocket.ssafast.dtospec.dto.request;
 
 import com.rocket.ssafast.dtospec.domain.element.DtoInfo;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class AddDtoSpecDto {
 
     private Long workspaceId;
